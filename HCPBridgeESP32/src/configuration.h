34bd7d3fa8e53cf19f17eab9be026367bab9c004
Gifford47 <gifford47@gmx.com>
2023-01-19 10:22:37 +0100

    changed mqtt topics to defines

--- HCPBridgeESP32/src/configuration.h
+++ HCPBridgeESP32/src/configuration.h
@@ -3,8 +3,36 @@ const char* AP_PASSWD = "MYPASSWORD";
 const char HOSTNAME[] = "HCPBridge";
 
 //MQTT
-const char* MQTTSERVER = "192.168.1.100";
+const char *MQTTSERVER = "192.168.1.100";
 const int MQTTPORT = 1883;
+#define FTOPIC "home/garage/door"
+#define AVAILABILITY_TOPIC FTOPIC "/availability"
+#define STATE_TOPIC FTOPIC "/state"
+#define CMD_TOPIC FTOPIC "/command"
+#define LAMP_TOPIC FTOPIC "/command/lamp"
+#define DOOR_TOPIC FTOPIC "/command/door"
+#define SENSOR_TOPIC FTOPIC "/sensor"
+
+#define HA_DISCOVERY_BIN_SENSOR "homeassistant/binary_sensor/garage/%s/config"
+#define HA_DISCOVERY_AV_SENSOR "homeassistant/sensor/garage/available/config"
+#define HA_DISCOVERY_SENSOR "homeassistant/sensor/garage/%s/config"
+#define HA_DISCOVERY_SWITCH "homeassistant/switch/garage/%s/config"
+#define HA_DISCOVERY_COVER "homeassistant/cover/garage/%s/config"
+
+const char *HA_ON = "true";
+const char *HA_OFF = "false";
+const char *HA_OPEN = "open";
+const char *HA_CLOSE = "close";
+const char *HA_HALF = "half";
+const char *HA_STOP = "stop";
+const char *HA_OPENING = "opening";
+const char *HA_CLOSING = "closing";
+const char *HA_CLOSED = "closed";
+const char *HA_OPENED = "open";
+const char *HA_ONLINE = "online";
+const char *HA_OFFLINE = "offline";
+
+const char *HA_VERSION = "0.0.2.0";
 
 //OTA Update
 const char* OTA_USERNAME = "admin";
